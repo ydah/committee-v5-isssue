0dbceed07dd1c00fdf8f92114c20490ecfab0660
ydah <13041216+ydah@users.noreply.github.com>
2024-02-13 20:08:27 +0900

    Add a reproduction code

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,41 +1,20 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: %i[ show update destroy ]
+  before_action :set_group, only: %i[ update ]
 
   # GET /groups
   def index
     @groups = Group.all
 
-    render json: @groups
-  end
-
-  # GET /groups/1
-  def show
-    render json: @group
-  end
-
-  # POST /groups
-  def create
-    @group = Group.new(group_params)
-
-    if @group.save
-      render json: @group, status: :created, location: @group
-    else
-      render json: @group.errors, status: :unprocessable_entity
-    end
+    render json: @groups, status: :ok
   end
 
   # PATCH/PUT /groups/1
   def update
     if @group.update(group_params)
-      render json: @group
+      render json: @group, status: :ok
     else
       render json: @group.errors, status: :unprocessable_entity
     end
-  end
-
-  # DELETE /groups/1
-  def destroy
-    @group.destroy!
   end
 
   private
